c499913969dbf85c396fa3a2e42eb38a9687bf4f
Loïc Goyet <loic@appventus.com>
2016-02-15 11:05:01 +0100

    rearrange method for the style-partial generator

--- style-partial/index.js
+++ style-partial/index.js
@@ -15,80 +15,75 @@ module.exports = generators.Base.extend({
             this.log(chalk.red('there is no frontlab.json file at the root of your project'));
         }
     },
-    askPartialType: function() {
+
+    prompting: function() {
         var done = this.async();
-        this.prompt({
-            type: 'list',
-            name: 'type',
-            message: 'What type of partial do you want to generate ?',
-            default: 'components',
-            choices: [
-                {
-                    name: 'base',
-                    value: 'base',
-                    checked: false
-                },
-                {
-                    name: 'components',
-                    value: 'components',
-                    checked: false
-                },
-                {
-                    name: 'layout',
-                    value: 'layout',
-                    checked: false
-                },
-                {
-                    name: 'pages',
-                    value: 'pages',
-                    checked: false
-                },
-                {
-                    name: 'themes',
-                    value: 'themes',
-                    checked: false
-                },
-                {
-                    name: 'utils',
-                    value: 'utils',
-                    checked: false
-                },
-                {
-                    name: 'vendor',
-                    value: 'vendor',
-                    checked: false
-                }
-            ]
-        }, function(answers) {
+        this.prompt([
+            {
+                type: 'list',
+                name: 'type',
+                message: 'What type of partial do you want to generate ?',
+                default: 'components',
+                choices: [
+                    {
+                        name: 'base',
+                        value: 'base',
+                        checked: false
+                    },
+                    {
+                        name: 'components',
+                        value: 'components',
+                        checked: false
+                    },
+                    {
+                        name: 'layout',
+                        value: 'layout',
+                        checked: false
+                    },
+                    {
+                        name: 'pages',
+                        value: 'pages',
+                        checked: false
+                    },
+                    {
+                        name: 'themes',
+                        value: 'themes',
+                        checked: false
+                    },
+                    {
+                        name: 'utils',
+                        value: 'utils',
+                        checked: false
+                    },
+                    {
+                        name: 'vendor',
+                        value: 'vendor',
+                        checked: false
+                    }
+                ]
+            },
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of your partial ?'
+            }
+        ], function(answers) {
             this.type = answers.type;
-            done();
-        }.bind(this));
-    },
-    askPartialName: function() {
-        var done = this.async();
-        this.prompt({
-            type: 'input',
-            name: 'name',
-            message: 'What is the name of your partial ?'
-        }, function(answers) {
             this.name = answers.name;
             done();
         }.bind(this));
     },
-    testGeneration: function() {
+
+    configuring: function() {
         this.file = this.destinationPath(this.config.sass.src + '/' + this.type + '/_' + this.name + '.scss');
         this.doGenerate = !fs.existsSync(this.file);
     },
-    generateFile: function() {
+
+    writing: function() {
         if (this.doGenerate) {
             fs.writeFileSync(this.file, '', 'utf8');
             this.log(chalk.green('create file : ') + this.file);
-        } else {
-            this.log(chalk.red('file ') + this.file + chalk.red(' already exists'));
-        }
-    },
-    importFile: function() {
-        if (this.doGenerate) {
+
             var injection = '@import \'' + this.type + '/' + this.name + '\';';
             var flag = '// END ' + this.type;
             var mainSCSSpath = this.destinationPath(this.config.sass.src + '/main.scss');
@@ -96,6 +91,8 @@ module.exports = generators.Base.extend({
             mainSCSS = mainSCSS.replace(flag, injection + '\n' + flag);
             fs.writeFileSync(mainSCSSpath, mainSCSS);
             this.log(chalk.green('import partial into : ') + mainSCSSpath);
+        } else {
+            this.log(chalk.red('file ') + this.file + chalk.red(' already exists'));
         }
-    }
+    },
 });
