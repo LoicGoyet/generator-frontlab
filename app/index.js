7cff3ba6dd8d7a0fec0d32667909fd0c747d9bc8
Loïc Goyet <loic@appventus.com>
2016-02-19 09:16:17 +0100

    restructure method order

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 var generators = require('yeoman-generator');
 var fs = require('fs-extra');
 var chalk = require('chalk');
-
 var config = require('../helper/config.js')();
+
 
 module.exports = generators.Base.extend({
     initializing: function() {
@@ -155,107 +155,112 @@ module.exports = generators.Base.extend({
         config.write(this, this.config);
     },
 
-    writing: function() {
-        // Generate package.json
-        // ---------------------
-        var packageExists = fs.existsSync(this.destinationPath('package.json'));
-        if (!packageExists) {
+    writing: {
+        writingPackageJSON: function() {
+            var packageExists = fs.existsSync(this.destinationPath('package.json'));
+            if (!packageExists) {
+                this.fs.copyTpl(
+                    this.templatePath('package.json'),
+                    this.destinationPath('package.json')
+                );
+            }
+
+            setTimeout(function() {
+                var scripts = {
+                    'make-dev': 'npm install && bower install && gulp',
+                    'start': 'gulp watch'
+                };
+
+                var package_str = fs.readFileSync(this.destinationPath('package.json'));
+                var package = JSON.parse(package_str);
+                package.scripts['make-dev'] = 'npm install && bower install && gulp';
+                package.scripts['start-dev'] = 'gulp watch';
+                fs.writeFileSync(this.destinationPath('package.json'), JSON.stringify(package, null, 4));
+            }.bind(this), 500);
+        },
+
+        writingDotfiles: function() {
             this.fs.copyTpl(
-                this.templatePath('package.json'),
-                this.destinationPath('package.json')
+                this.templatePath('_scss-lint.yml'),
+                this.destinationPath('.scss-lint.yml')
             );
-        }
 
-        setTimeout(function() {
-            var scripts = {
-                'make-dev': 'npm install && bower install && gulp',
-                'start': 'gulp watch'
-            };
-
-            var package_str = fs.readFileSync(this.destinationPath('package.json'));
-            var package = JSON.parse(package_str);
-            package.scripts['make-dev'] = 'npm install && bower install && gulp';
-            package.scripts['start-dev'] = 'gulp watch';
-            fs.writeFileSync(this.destinationPath('package.json'), JSON.stringify(package, null, 4));
-        }.bind(this), 500);
-
-
-        // Create dotfiles
-        // -----------------
-        this.fs.copyTpl(
-            this.templatePath('_scss-lint.yml'),
-            this.destinationPath('.scss-lint.yml')
-        );
-
-        this.fs.copyTpl(
-            this.templatePath('_editorconfig'),
-            this.destinationPath('.editorconfig')
-        );
-
-        // Create gulpfile
-        // ---------------
-        this.fs.copyTpl(
-            this.templatePath('gulpfile.js'),
-            this.destinationPath('gulpfile.js'),
-            { config: this.config }
-        );
-
-        // Create sass files
-        // -----------------
-        this.fs.copyTpl(
-            this.templatePath('sass'),
-            this.destinationPath(this.config.sass_src)
-        );
-
-        // Create twig files
-        // -----------------
-        if (this.config.twig_enabled) {
             this.fs.copyTpl(
-                this.templatePath('templates'),
-                this.destinationPath(this.config.twig_src)
+                this.templatePath('_editorconfig'),
+                this.destinationPath('.editorconfig')
             );
-        }
+        },
 
-        // Create public folder
-        // --------------------
-        if (this.config.public_enabled) {
+        writingGulpfile: function() {
             this.fs.copyTpl(
-                this.templatePath('public'),
-                this.destinationPath(this.config.public_src + '/public')
+                this.templatePath('gulpfile.js'),
+                this.destinationPath('gulpfile.js'),
+                { config: this.config }
             );
-        }
+        },
+
+        writingSassArchitecture: function() {
+            this.fs.copyTpl(
+                this.templatePath('sass'),
+                this.destinationPath(this.config.sass_src)
+            );
+        },
+
+        writingTwigArchitecture: function() {
+            if (this.config.twig_enabled) {
+                this.fs.copyTpl(
+                    this.templatePath('templates'),
+                    this.destinationPath(this.config.twig_src)
+                );
+            }
+        },
+
+        writingPublicArchitecture: function() {
+            if (this.config.public_enabled) {
+                this.fs.copyTpl(
+                    this.templatePath('public'),
+                    this.destinationPath(this.config.public_src + '/public')
+                );
+            }
+        },
     },
 
-    install: function() {
-        var devDependencies = [
-            'gulp',
-            'gulp-size',
-            'gulp-load-plugins',
-            'fs-extra',
-            'run-sequence',
+    install: {
+        getDependencies: function() {
+            this.devDependencies = [
+                'gulp',
+                'gulp-size',
+                'gulp-load-plugins',
+                'fs-extra',
+                'run-sequence',
 
-            // Gulp sass dependencies
-            'gulp-sourcemaps',
-            'gulp-sass',
-            'gulp-autoprefixer',
-        ];
+                // Gulp sass dependencies
+                'gulp-sourcemaps',
+                'gulp-sass',
+                'gulp-autoprefixer',
+            ];
+        },
 
-        // Gulp twig dependencies
-        if (this.config.twig_compilation) {
-            devDependencies.push(
-                'gulp-twig',
-                'gulp-ext-replace',
-                'gulp-prettify'
-            );
-        }
+        addTwigDependencies: function() {
+            if (this.config.twig_compilation) {
+                this.devDependencies.push(
+                    'gulp-twig',
+                    'gulp-ext-replace',
+                    'gulp-prettify'
+                );
+            }
+        },
 
-        // Gulp serve dependencies
-        if (this.config.serve) {
-            devDependencies.push(
-                'browser-sync'
-            );
-        }
+        addServeDependencies: function() {
+            if (this.config.serve) {
+                this.devDependencies.push(
+                    'browser-sync'
+                );
+            }
+        },
 
-        this.npmInstall(devDependencies, { 'saveDev': true });
+        installDependencies: function() {
+            this.npmInstall(this.devDependencies, { 'saveDev': true });
+        },
     },
 });
